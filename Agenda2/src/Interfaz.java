def231f95b80a14ee333ebbc5b522295c5b9a7cc
GermanGiorgis <germanggiorgis@gmail.com>
2024-04-23 19:00:58 -0300

    Agregando nuevo proyecto (Agenda2)

---
+++ Agenda2/src/Interfaz.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Interfaz {
+    private JPanel Panel;
+    private JTextField Generotext;
+    private JTextField Nombretext;
+    private JTextField Apellidotext;
+    private JTextField DNItext;
+    private JTextField Mailtext;
+    private JButton BotonConcultar;
+    private JButton Botonagregar;
+    private JLabel Titulo;
+    private JList Lista;
+
+}
